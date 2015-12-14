65bc087c6378efcd885756f25ba579780ca0a387
Mahroza Pradana <mahrozapradana46@gmail.com>
2015-12-14 16:54:47 +0700

    [ADD] estate_nursery : add sum for report main nursery and pre nursery

--- estate_nursery/report_plantation.py
+++ estate_nursery/report_plantation.py
@@ -32,8 +32,6 @@ class ReportLine(models.Model):
     batch_ids = fields.One2many("estate.nursery.batch","reportline_id","selection")
     selection_ids = fields.One2many("estate.nursery.selection","reportline_id","selection")
     selectionline_ids = fields.One2many("estate.nursery.selectionline","reportline_id","selection")
-    total_do = fields.Integer("Total DO",store=True,compute="_get_total")
-    total_planted = fields.Integer("Total planted",store=True,compute="_get_total_planted")
     kebun_location_id = fields.Many2one('stock.location',"Estate Location",
                                           domain=[('estate_location', '=', True),
                                                   ('estate_location_level', '=', '1'),
@@ -48,26 +46,5 @@ class ReportLine(models.Model):
         today = datetime.now()
         self.report_date=today
 
-    #total DO
-    @api.one
-    @api.depends("total_do")
-
-    def _get_total(self):
-        batchids = self.batch_ids
-        total_do = self.total_do
-        for item in batchids:
-            total_do += item.qty_received
-            print total_do
-
-    #total planted
-    @api.one
-    @api.depends("total_planted","batch_ids")
-
-    def _get_total_planted(self):
-        batch_ids = self.batch_ids
-        total_planted = self.total_planted
-        for item in batch_ids:
-            total_planted += item.qty_planted
-            print total_planted
 
 
