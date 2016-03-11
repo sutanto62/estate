700c1cb0f1821b634989e3317168426cd0a31071
Cayadi Sutanto <cayadi.sutanto@palmagroup.co.id>
2016-03-11 10:49:35 +0700

    [fix] hr_indonesia: #364 models not loaded

--- estate/__openerp__.py
+++ estate/__openerp__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock', 'hr', 'hr_indonesia'],
+    'depends': ['base', 'stock', 'hr_indonesia'],
 
     # always loaded
     'data': [
