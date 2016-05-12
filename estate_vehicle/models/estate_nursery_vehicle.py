145581c6dfe42ee5f27b9ff0a5adfa65a9c71333
mahroza pradana <mahrozapradana46@gmail.com>
2016-05-12 10:44:22 +0700

    [add] estate_vehicle : add menu capacity and status car

--- estate_vehicle/models/estate_nursery_vehicle.py
+++ estate_vehicle/models/estate_nursery_vehicle.py
@@ -10,11 +10,13 @@ import calendar
 class NurseryVehicle(models.Model):
 
     _inherit="fleet.vehicle"
-
+    _description = "inherit information detail to fleet management"
 
     no_vehicle=fields.Char('No Vehicle')
     vehicle_type=fields.Selection([('1','Vehicle Internal'), ('2','Vehicle External')])
     employee_driver_id=fields.Many2one('hr.employee')
+    capacity_vehicle = fields.Integer('Capacity')
+    status_vehicle = fields.Selection([('1','Available'), ('2','Breakdown')])
 
 
 
