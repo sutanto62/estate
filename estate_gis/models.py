82c26df26b0497f6b70cd986edb9bf7d0275f16e
Cayadi Sutanto <cayadi.sutanto@palmagroup.co.id>
2016-05-24 17:36:46 +0700

    [ADD] estate_gis: block need to display geom.

---
+++ estate_gis/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class estate_gis(models.Model):
+#     _name = 'estate_gis.estate_gis'
+
+#     name = fields.Char()
---
+++ estate_gis/models/inherited_estate.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, osv
+from openerp.addons.base_geoengine import geo_model
+from openerp.addons.base_geoengine import fields as geo_fields
+
+class EstateBlockTemplate(geo_model.GeoModel):
+    _inherit = 'estate.block.template'
+
+    the_area = geo_fields.GeoMultiPolygon('Area', help='Imported from GIS')
