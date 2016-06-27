8f451f1ebc707a33cd475964f91f554c2ac96299
mahroza pradana <mahrozapradana46@gmail.com>
2016-06-27 12:55:49 +0700

    [ADD] estate_workshop : change query to join with summary vehicle

--- estate_workshop/models/estate_cost_workshop.py
+++ estate_workshop/models/estate_cost_workshop.py
@@ -15,71 +15,75 @@ class ViewTotalAmountTimesheetMecanic(models.Model):
     _order='mo_id'
 
     id = fields.Integer()
+    mo_id = fields.Integer()
     total_amount = fields.Float()
+    year_log = fields.Char()
+    month_log = fields.Char()
+    vehicle_id = fields.Many2one('fleet.vehicle')
 
     def init(self, cr):
         cr.execute("""create or replace view view_timesheet_mecanic_totalamounts as
-select row_number() over()id,a.mo_id mo_id, sum(a.total_amount) total_amount from (
-        select row_number() over()id,mo_id,
-               total_time_per_day,
-               hourly_wage,
-               daily_wage,count(*)"count",
-               hour,
-               CASE
-                WHEN total_time_per_day < hour THEN (total_time_per_day*hourly_wage)
-                WHEN total_time_per_day >= hour THEN (daily_wage)
-                END total_amount from (
-        select (month_log::text||year_log::text||asset::text||mo_id::text)::Integer parent_id,mo_id,
-        sum(total_time) as total_time_per_day,hourly_wage,daily_wage,day,hour from(
-        select
-            date_part('month', create_date) month_log,
-            date_part('year', create_date) year_log,
-            mo_id,
-            asset,
-            create_date,
-            c.mastertask_id,
-            activity_id,
-            start_time,
-            end_time ,
-            (end_time-start_time) as total_time,
-            wage,
-            weekly_wage,
-            daily_wage,
-            hourly_wage,
-            day,
-            hour
-            from (
-            select
-                asset_id as asset,
-                create_date,mo_id,
-                mastertask_id,employee_id,activity_id,start_time,end_time
-                from estate_mecanic_timesheet mt
-                inner join (
-                select mo.id as mo_id,et_id,employee_id,activity_id,start_time,end_time from mro_order mo inner join(
-                select
-                    et.id as et_id,
-                    employee_id,
-                    activity_id,
-                    start_time,
-                    end_time,owner_id
-                from estate_timesheet_activity_transport et where dc_type is null
-                )moa on mo.id = moa.owner_id group by mo_id,et_id,employee_id,activity_id,start_time,end_time
-                )etat on mt.timesheet_id=etat.et_id
-                )c left join (
-                select
-                    hre.id,
-                    wage,
-                    weekly_wage,
-                    daily_wage,
-                    hourly_wage,
-                    day,
-                    hour from hr_employee hre right join hr_contract hrc on hre.id=hrc.employee_id
-               	)hrcon on c.employee_id = hrcon.id
-                )b group by day, hour,hourly_wage,daily_wage,month_log,year_log,asset,mo_id)a group by mo_id,total_time_per_day,
-               hourly_wage,
-               daily_wage,total_amount,
-               hour
-		) a group by a.mo_id""")
+                select row_number() over()id,a.mo_id mo_id, sum(a.total_amount) total_amount,month_log,year_log,vehicle_id from (
+                        select row_number() over()id,mo_id,
+                               total_time_per_day,
+                               hourly_wage,
+                               daily_wage,count(*)"count",month_log,year_log,vehicle_id,
+                               hour,
+                               CASE
+                                WHEN total_time_per_day < hour THEN (total_time_per_day*hourly_wage)
+                                WHEN total_time_per_day >= hour THEN (daily_wage)
+                                END total_amount from (
+                        select (month_log::text||year_log::text||asset::text||mo_id::text)::Integer parent_id,mo_id,month_log,year_log,vehicle_id,
+                        sum(total_time) as total_time_per_day,hourly_wage,daily_wage,day,hour from(
+                        select
+                            date_part('month', create_date) month_log,
+                            date_part('year', create_date) year_log,
+                            mo_id,asset,
+                            vehicle_id,
+                            create_date,
+                            c.mastertask_id,
+                            activity_id,
+                            start_time,
+                            end_time ,
+                            (end_time-start_time) as total_time,
+                            wage,
+                            weekly_wage,
+                            daily_wage,
+                            hourly_wage,
+                            day,
+                            hour
+                            from (
+                            select
+                                vehicle_id,asset_id asset,
+                                create_date,mo_id,
+                                mastertask_id,employee_id,activity_id,start_time,end_time
+                                from estate_mecanic_timesheet mt
+                                inner join (
+                                select mo.id as mo_id,et_id,employee_id,activity_id,start_time,end_time,vehicle_id from mro_order mo inner join(
+                                select
+                                    et.id as et_id,
+                                    employee_id,
+                                    activity_id,
+                                    start_time,
+                                    end_time,owner_id,vehicle_id
+                                from estate_timesheet_activity_transport et where dc_type is null
+                                )moa on mo.id = moa.owner_id group by mo_id,et_id,employee_id,activity_id,start_time,end_time,vehicle_id
+                                )etat on mt.timesheet_id=etat.et_id
+                                )c left join (
+                                select
+                                    hre.id,
+                                    wage,
+                                    weekly_wage,
+                                    daily_wage,
+                                    hourly_wage,
+                                    day,
+                                    hour from hr_employee hre right join hr_contract hrc on hre.id=hrc.employee_id
+                                )hrcon on c.employee_id = hrcon.id
+                                )b group by day, hour,hourly_wage,daily_wage,month_log,year_log,asset,vehicle_id,mo_id)a group by mo_id,total_time_per_day,
+                               hourly_wage,
+                               daily_wage,total_amount,
+                               hour,month_log,year_log,vehicle_id
+                        ) a group by a.mo_id,month_log,year_log,vehicle_id """)
 
 
 class ViewTotalCostDetailWorkshopSparepart(models.Model):
@@ -92,23 +96,28 @@ class ViewTotalCostDetailWorkshopSparepart(models.Model):
     id = fields.Integer()
     mo_id = fields.Integer()
     total_amount = fields.Float()
+    year_log = fields.Char()
+    month_log = fields.Char()
+    vehicle_id = fields.Many2one('fleet.vehicle')
 
     def init(self, cr):
         cr.execute("""create or replace view view_cost_workshop_sparepart as
-                select row_number() over()id,mo_id, sum(total_cost) as total_amount from(
+                select row_number() over()id,mo_id, sum(total_cost) as total_amount,fleet_id as vehicle_id,month_log,year_log from(
                 select row_number() over()id,
                     (month_log::text||year_log::text||asset_id::text||mo_id::text)::Integer parent_id,
                     mo_id,
-                    asset_id,
+                    asset_id,fleet_id,month_log,year_log,
                     b.product_id,
                     qty_product,
                     cost ,
                     (qty_product*cost) as total_cost
                 from(
-                    select mo.id,date_part('day',mo.create_date) day_log,mo.id as mo_id,
+                    select mo.id,fleet_id,date_part('day',mo.create_date) day_log,mo.id as mo_id,
                                         date_part('month',mo.create_date) month_log,
                                         date_part('year',mo.create_date) year_log
-                                        ,asset_id,product_id,qty_product,cost from mro_order mo
+                                        ,asset_id,ewas.product_id,qty_product,cost
+                                        from mro_order mo
+                                      	inner join asset_asset aa on aa.id = mo.asset_id
                 inner join (
                     select owner_id,product_id,qty_product,cost from estate_workshop_actual_sparepart ewas
                 inner join (
@@ -118,7 +127,9 @@ class ViewTotalCostDetailWorkshopSparepart(models.Model):
                 inner join (
                         select * from product_price_history)pph on pt.id=pph.product_template_id )a on a.id = pp.product_tmpl_id
                 )c on ewas.product_id = c.id
-                )ewas on mo.id = ewas.owner_id)b group by month_log,year_log,asset_id,mo_id,product_id,qty_product,cost)a group by mo_id""")
+                )ewas on mo.id = ewas.owner_id)b
+                group by month_log,year_log,asset_id,mo_id,product_id,qty_product,cost,fleet_id,month_log,year_log)a
+                group by mo_id,fleet_id,month_log,year_log""")
 
 class ViewCostTotalWorkshop(models.Model):
 
@@ -132,18 +143,21 @@ class ViewCostTotalWorkshop(models.Model):
     type_log = fields.Char()
     count = fields.Integer()
     total_amount =fields.Float()
+    year_log = fields.Char()
+    month_log = fields.Char()
+    vehicle_id = fields.Many2one('fleet.vehicle')
 
     def init(self, cr):
         cr.execute("""create or replace view v_summary_cost_workshop_detail as
             select detail.* from(
-                select row_number() over()id,type_log,mo_id,"count",total_amount from(
-                select 'Cost Labour' as type_log,mo_id,count(*)"count",sum(total_amount) as total_amount from(
+                select row_number() over()id,type_log,mo_id,"count",total_amount,year_log,month_log,vehicle_id from(
+                select 'Cost Labour' as type_log,mo_id,count(*)"count",sum(total_amount) as total_amount,year_log,month_log,vehicle_id from(
                 select * from view_timesheet_mecanic_totalamounts
-            )cl group by mo_id
+                )cl group by mo_id,year_log,month_log,vehicle_id
             union
-                select 'Cost Sparepart' as type_log,mo_id,count(*)"count",sum(total_amount) as total_amount from(
-                select * from view_cost_workshop_sparepart
-                )cs group by mo_id
+            select 'Cost Sparepart' as type_log,mo_id,count(*)"count",sum(total_amount) as total_amount,year_log,month_log,vehicle_id from(
+            select * from view_cost_workshop_sparepart
+                )cs group by mo_id,year_log,month_log,vehicle_id
                 )utot)detail
             """)
 
