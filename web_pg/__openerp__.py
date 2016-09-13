f1bff8d2b6897e084d0e5ea25f242104d0d10333
Cayadi Sutanto <cayadi.sutanto@palmagroup.co.id>
2016-09-13 10:02:56 +0700

    [ADD] estate: UI and fingerprint improvement
    * Add description index html.
    * Skip wage check if activity line empty.
    * Validate imported fingerprint using name and NIK.

---
+++ web_pg/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web Custom",
+
+    'summary': """
+        Logo, Favicon""",
+
+    'description': """
+        Modification default web template
+    """,
+
+    'author': "Palma Group",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Web',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
