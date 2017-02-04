a2c01f96c83aa3afabd0c66233c2703904365a4b
mahroza pradana <mahrozapradana46@gmail.com>
2017-02-04 07:04:21 +0700

    [ADD] purchase_service_receipt_note : add category service in purchase request link

--- purchase_service_receipt_note/models/inherit_purchase_request_line.py
+++ purchase_service_receipt_note/models/inherit_purchase_request_line.py
@@ -18,12 +18,50 @@ class InheritPurchaseRequestLine(models.Model):
 
     _inherit = 'purchase.request.line'
 
-    # @api.multi
-    # @api.onchange('request_id','product_id')
-    # def _onchange_product_purchase_request_line(self):
-    #
-    #     for item in self:
-    #
-    #         if item.request_id.type_product == 'service':
-    #
-    #             product_id = item.env['product.template'].search([])
+    @api.multi
+    @api.onchange('request_id','product_id')
+    def _onchange_product_purchase_request_line(self):
+        #use to onchange domain product same as product_category
+        for item in self:
+            if item.request_id.type_functional and item.request_id.department_id:
+                arrProductCateg = []
+                mappingFuntional = item.env['mapping.department.product'].search([('type_functional','=',item.request_id.type_functional),
+                                                                                  ('department_id.id','=',item.request_id.department_id.id)])
+
+                for productcateg in mappingFuntional:
+                    arrProductCateg.append(productcateg.product_category_id.id)
+
+                arrProdCatId = []
+                prod_categ = item.env['product.category'].search([('parent_id','in',arrProductCateg)])
+
+                for productcategparent in prod_categ:
+                    arrProdCatId.append(productcategparent.id)
+
+                if prod_categ:
+                    if item.type_product == 'service':
+                        return  {
+                            'domain':{
+                                'product_id':[('categ_id','in',arrProdCatId),('type','=','service')]
+                                 }
+                            }
+                    else:
+                        return  {
+                            'domain':{
+                                'product_id':[('categ_id','in',arrProdCatId)]
+                                 }
+                            }
+                elif prod_categ != ():
+                    if item.type_product == 'service':
+
+                        return  {
+                        'domain':{
+                            'product_id':[('categ_id','in',arrProductCateg),('type','=','service')]
+                             }
+                        }
+
+                    else:
+                        return  {
+                        'domain':{
+                            'product_id':[('categ_id','in',arrProductCateg)]
+                             }
+                        }
