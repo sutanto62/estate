c5f920fe53d25e86cc246fe995a69b70c1c2be08
mahroza pradana <mahrozapradana46@gmail.com>
2017-02-22 11:08:54 +0700

    [ADD] purchase_indonesia : add locale in quotation_comparison form

--- purchase_indonesia/models/quotation_comparison_form.py
+++ purchase_indonesia/models/quotation_comparison_form.py
@@ -486,7 +486,7 @@ class QuotationComparisonForm(models.Model):
 
                 #convert Goods Price to Currency IDR indonesia
                 company_currency_name = item.env['res.company'].search([('id','=',item.company_id.id)]).currency_id.name
-                price = babel.numbers.format_currency( decimal.Decimal(goods_price), company_currency_name)
+                price = babel.numbers.format_currency( decimal.Decimal(goods_price), company_currency_name,locale='id_ID')
 
                 purchase = item.env['purchase.order'].search([('id','=',record.order_id.id)])
                 for record in purchase:
