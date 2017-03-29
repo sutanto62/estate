70278f27ed79b743d3e0e47f77666dfce52a12ef
Cayadi Sutanto <cayadi.sutanto@palmagroup.co.id>
2017-03-29 13:59:10 +0700

    [IMP] crosstab low performance

--- hr_fingerprint_ams/models/labour_fingerprint.py
+++ hr_fingerprint_ams/models/labour_fingerprint.py
@@ -76,58 +76,99 @@ class LabourFingerprint(models.Model):
             """)
 
         tools.drop_view_if_exists(cr, 'hr_fingerprint_ams_fingerprint')
-        cr.execute("""
+        cr.execute(
+            """
             CREATE or REPLACE VIEW hr_fingerprint_ams_fingerprint
             as
-            SELECT
-                row_number() over (order by ct.row_name) as id,
-                ct.row_name as name,
-                substring(ct.row_name from position('/'  in ct.row_name)+1 for char_length(ct.row_name))::date as date,
-                ct.sign_in,
-                ct.sign_out,
-                ee.id as employee_id,
-                tm.id as team_id,
-                t.assistant_id,
-                (date_part('hour', ct.sign_out - ct.sign_in)+(date_part('minute', ct.sign_out - ct.sign_in)/60)::numeric(4,2)) as worked_hours,
+            select
+                row_number() over (order by concat(a.employee_id,'/',a.upkeep_date)) as id,
+                concat(a.employee_id,'/',a.upkeep_date) as name,
+                c.name as sign_in,
+                d.name as sign_out,
+                a.employee_id,
+                a.upkeep_team_id as team_id,
+                b.employee_id as assistant_id,
+                (date_part('hour', d.name - c.name)+(date_part('minute', d.name - c.name)/60)::numeric(4,2)) as worked_hours,
                 total_number_of_day(
-                    substring(ct.row_name from 1 for position('/' in ct.row_name)-1)::int,
-                    substring(ct.row_name from position('/' in ct.row_name)+1 for char_length(ct.row_name))::date
-                ) as number_of_day,
-                CASE WHEN fa.state isnull THEN 'attendance'
-                    ELSE fa.state
-                END
-            FROM crosstab(
-                $$
-                SELECT a.row_name, /* a.name_related, a.upkeep_date, a.hk, */ b.action::text, b.name FROM (
-                    SELECT
-                        concat(l.employee_id,'/', l.upkeep_date) as row_name,
-                        l.upkeep_date,
-                        e.name_related,
-                        sum(l.number_of_day) as hk
-                    FROM estate_upkeep_labour l
-                    LEFT JOIN hr_employee e on l.employee_id = e.id
-                    WHERE
-                        l.number_of_day > 0
-                    GROUP BY l.employee_id, l.upkeep_date, e.name_related
-                ) a
-                LEFT JOIN (
-                    SELECT
-                        concat(ee.id, '/', fa.date) as row_name,
-                        ha.action,
-                        ha.name,
-                        ha.worked_hours
-                    FROM hr_fingerprint_ams_attendance fa
-                    LEFT JOIN hr_employee ee on lower(fa.employee_name) = lower(ee.name_related)
-                    LEFT JOIN hr_attendance ha on ha.finger_attendance_id = fa.id
-                ) b on a.row_name = b.row_name
-                ORDER BY a.name_related, a.upkeep_date, b.name;
-                $$
-                ) as ct(row_name text, sign_in timestamp, sign_out timestamp)
-            LEFT JOIN hr_employee ee on substring(ct.row_name from 1 for position('/' in ct.row_name)-1)::int = ee.id
-            LEFT JOIN estate_hr_member tm on tm.employee_id = ee.id
-            LEFT JOIN estate_hr_team t on tm.team_id = t.id
-            LEFT JOIN hr_fingerprint_ams_attendance fa on (lower(ee.name_related) = lower(fa.employee_name)) AND
-            (fa.date = substring(ct.row_name from position('/'  in ct.row_name)+1 for char_length(ct.row_name))::date)
-            ;
+                                substring(concat(a.employee_id,'/',a.upkeep_date) from 1 for position('/' in concat(a.employee_id,'/',a.upkeep_date))-1)::int,
+                                substring(concat(a.employee_id,'/',a.upkeep_date) from position('/' in concat(a.employee_id,'/',a.upkeep_date))+1 for char_length(concat(a.employee_id,'/',a.upkeep_date)))::date
+                            ) as number_of_day
+            from estate_upkeep_labour a
+            join estate_hr_team b on a.upkeep_team_id = b.id
+            join (
+                select
+                    cc.employee_id,
+                    cc.name
+                from hr_attendance cc
+                    where cc.action = 'sign_in'
+                ) c on concat(a.employee_id,'/', (a.upkeep_date at time zone 'UTC')::date) = concat(c.employee_id,'/', c.name::date) -- convert to utc
+            join (
+                select
+                    dd.employee_id,
+                    dd.name
+                from hr_attendance dd
+                    where dd.action = 'sign_out'
+                ) d on concat(a.employee_id,'/', a.upkeep_date) = concat(d.employee_id,'/', d.name::date)  -- do not convert to utc
+            where
+                a.employee_id = c.employee_id
+            order by concat(a.employee_id,'/',a.upkeep_date);
+
             """
         )
+
+        # Query took 4 minutes 33 seconds
+        # cr.execute("""
+        #     CREATE or REPLACE VIEW hr_fingerprint_ams_fingerprint
+        #     as
+        #     SELECT
+        #         row_number() over (order by ct.row_name) as id,
+        #         ct.row_name as name,
+        #         substring(ct.row_name from position('/'  in ct.row_name)+1 for char_length(ct.row_name))::date as date,
+        #         ct.sign_in,
+        #         ct.sign_out,
+        #         ee.id as employee_id,
+        #         tm.id as team_id,
+        #         t.assistant_id,
+        #         (date_part('hour', ct.sign_out - ct.sign_in)+(date_part('minute', ct.sign_out - ct.sign_in)/60)::numeric(4,2)) as worked_hours,
+        #         total_number_of_day(
+        #             substring(ct.row_name from 1 for position('/' in ct.row_name)-1)::int,
+        #             substring(ct.row_name from position('/' in ct.row_name)+1 for char_length(ct.row_name))::date
+        #         ) as number_of_day,
+        #         CASE WHEN fa.state isnull THEN 'attendance'
+        #             ELSE fa.state
+        #         END
+        #     FROM crosstab(
+        #         $$
+        #         SELECT a.row_name, /* a.name_related, a.upkeep_date, a.hk, */ b.action::text, b.name FROM (
+        #             SELECT
+        #                 concat(l.employee_id,'/', l.upkeep_date) as row_name,
+        #                 l.upkeep_date,
+        #                 e.name_related,
+        #                 sum(l.number_of_day) as hk
+        #             FROM estate_upkeep_labour l
+        #             LEFT JOIN hr_employee e on l.employee_id = e.id
+        #             WHERE
+        #                 l.number_of_day > 0
+        #             GROUP BY l.employee_id, l.upkeep_date, e.name_related
+        #         ) a
+        #         LEFT JOIN (
+        #             SELECT
+        #                 concat(ee.id, '/', fa.date) as row_name,
+        #                 ha.action,
+        #                 ha.name,
+        #                 ha.worked_hours
+        #             FROM hr_fingerprint_ams_attendance fa
+        #             LEFT JOIN hr_employee ee on lower(fa.employee_name) = lower(ee.name_related)
+        #             LEFT JOIN hr_attendance ha on ha.finger_attendance_id = fa.id
+        #         ) b on a.row_name = b.row_name
+        #         ORDER BY a.name_related, a.upkeep_date, b.name;
+        #         $$
+        #         ) as ct(row_name text, sign_in timestamp, sign_out timestamp)
+        #     LEFT JOIN hr_employee ee on substring(ct.row_name from 1 for position('/' in ct.row_name)-1)::int = ee.id
+        #     LEFT JOIN estate_hr_member tm on tm.employee_id = ee.id
+        #     LEFT JOIN estate_hr_team t on tm.team_id = t.id
+        #     LEFT JOIN hr_fingerprint_ams_attendance fa on (lower(ee.name_related) = lower(fa.employee_name)) AND
+        #     (fa.date = substring(ct.row_name from position('/'  in ct.row_name)+1 for char_length(ct.row_name))::date)
+        #     ;
+        #     """
+        # )
