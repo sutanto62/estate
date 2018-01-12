127880d8d0d0f3b458489161b72c77953ef426fd
pwidodo <probo.sukmohadi@palmagrou.co.id>
2018-01-12 09:20:19 +0700

    [HOTFIX] #750 Premi pengganti HK dg K < 20 (add check new record or existing)

--- estate_hk_21/models/inherit_estate_upkeep.py
+++ estate_hk_21/models/inherit_estate_upkeep.py
@@ -24,20 +24,48 @@ class InheritUpkeepLabour(models.Model):
         if start:
             end = (start + relativedelta(months=+1, days=-1))
 
+#         upkeep_ids = self.env['estate.upkeep.labour'].search([('employee_id', '=', self.employee_id.id),
+#                                                               ('upkeep_date', '>=', start),
+#                                                               ('upkeep_date', '<=', end),
+#                                                               ('attendance_code_id', 'in', att_code)])
+# 
+#         regular_number_of_days = sum(item.number_of_day for item in upkeep_ids) + (self.number_of_day if not self.attendance_code_id.piece_rate else 0) 
+#         
+#         if not self.attendance_code_id.piece_rate and (regular_number_of_days > monthly_limit):
+#             # check if month to date exceed monthly limit.
+#             err_msg = _('%s will exceed  %s number of days. Use piece rate attendance code.' % (self.employee_id.name, monthly_limit))
+#             raise ValidationError(err_msg)
+#         elif self.attendance_code_id.piece_rate and (regular_number_of_days < monthly_limit):
+#             # prevent piece rate attendance code used before total exceed monthly limit
+#             err_msg = _('%s has not exceed %s number of days yet. Use regular attendance' % (self.employee_id.name, monthly_limit))
+#             raise ValidationError(err_msg)
+
         upkeep_ids = self.env['estate.upkeep.labour'].search([('employee_id', '=', self.employee_id.id),
-                                                              ('upkeep_date', '>=', start),
-                                                              ('upkeep_date', '<=', end),
-                                                              ('attendance_code_id', 'in', att_code)])
+                                                          ('upkeep_date', '>=', start),
+                                                          ('upkeep_date', '<=', end)])
 
-        regular_number_of_days = sum(item.number_of_day for item in upkeep_ids) + (self.number_of_day if not self.attendance_code_id.piece_rate else 0) 
-
-        if not self.attendance_code_id.piece_rate and (regular_number_of_days > monthly_limit):
-            # check if month to date exceed monthly limit.
-            err_msg = _('%s will exceed  %s number of days. Use piece rate attendance code.' % (self.employee_id.name, monthly_limit))
-            raise ValidationError(err_msg)
-        elif self.attendance_code_id.piece_rate and (regular_number_of_days <= 20):
-            # prevent piece rate attendance code used before total exceed monthly limit
-            err_msg = _('%s has not exceed %s number of days yet. Use regular attendance' % (self.employee_id.name, monthly_limit))
-            raise ValidationError(err_msg)
-
+        regular_number_of_days_hk = sum(item.number_of_day for item in upkeep_ids if item.attendance_code_id.piece_rate != True)
+        regular_number_of_days_pr = sum(item.number_of_day for item in upkeep_ids if item.attendance_code_id.piece_rate == True) 
+        
+        new_record = True if sum(item.number_of_day for item in upkeep_ids if item.id == self.id) == 0 else False
+        
+#         err_msg = _('HK %s  PR %s Existing %s' % (regular_number_of_days_hk, regular_number_of_days_pr, new_record))
+        
+        if new_record:
+            if self.attendance_code_id.piece_rate != True:
+                if regular_number_of_days_hk + self.number_of_day >  monthly_limit: 
+                    err_msg = _('%s will exceed  %s number of days. Use piece rate attendance code.' % (self.employee_id.name, monthly_limit))
+                    raise ValidationError(err_msg)
+            else:
+                if regular_number_of_days_hk < monthly_limit: 
+                    err_msg = _('%s has not exceed %s number of days yet. Use regular attendance' % (self.employee_id.name, monthly_limit))
+                    raise ValidationError(err_msg)        
+        else:
+            if regular_number_of_days_hk > monthly_limit:
+                err_msg = _('%s will exceed  %s number of days. Use piece rate attendance code.' % (self.employee_id.name, monthly_limit))
+                raise ValidationError(err_msg)
+            if regular_number_of_days_hk <= monthly_limit and regular_number_of_days_pr > 0: 
+                    err_msg = _('%s has not exceed %s number of days yet. Use regular attendance' % (self.employee_id.name, monthly_limit))
+                    raise ValidationError(err_msg)
+        
         return super(InheritUpkeepLabour, self)._check_attendance_code()
