ef9e09aa2870706c6511d99e95d331416d4d2e03
pwidodo <probo.sukmohadi@palmagrou.co.id>
2018-04-05 11:54:43 +0700

    [INIT] estate_ffb and estate_payrollffb for harvesting module

---
+++ estate_ffb/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Estate FFB (Fresh Fruit Bunches)",
+
+    'summary': """
+        Harvesting Administration""",
+
+    'description': """
+        As agronomy, they want to administer result of ffb so that they can get quantity and control quality of its ffb and harvesting process.
+    """,
+
+    'author': "Cayadi Sutanto <cayadi.sutanto@palmagroup.co.id>, Probo Sukmohadi <probo.sukmohadi@palmagroup.co.id>, Ardian Pramana <ardian.pramana@palmagroup.co.id",
+    'website': "http://www.palmagroup.co.id",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Agriculture',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['estate'],
+
+    # always loaded
+    'data': [
+        
+    ],
+    # only loaded in demonstration mode (prerequisite data should be processed first)
+    'demo': [
+        
+    ],
+}
---
+++ estate_payrollffb/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Estate Payroll FFB (Fresh Fruit Bunches)",
+
+    'summary': """
+        Harvesting Labour Payroll Calculation""",
+
+    'description': """
+        As human resource and agronomy, they want to administer productivity of harvesting labour so that they can control the harvesting process and calculate exact payroll based on labour's productivity.
+    """,
+
+    'author': "Cayadi Sutanto <cayadi.sutanto@palmagroup.co.id>, Probo Sukmohadi <probo.sukmohadi@palmagroup.co.id>, Ardian Pramana <ardian.pramana@palmagroup.co.id",
+    'website': "http://www.palmagroup.co.id",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Agriculture',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['estate', 'estate_payroll', 'estate_ffb'],
+
+    # always loaded
+    'data': [
+        
+    ],
+    # only loaded in demonstration mode (prerequisite data should be processed first)
+    'demo': [
+        
+    ],
+}
