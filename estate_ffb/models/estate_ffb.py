3c068a9b03b1ef40f37735d925ac3c8abe324162
Ardian Pramana <pramana.ardian@gmail.com>
2018-04-09 16:27:39 +0700

    [ADD] estate_ffb: #832 add filter by confirm, add group by company, estate [ADD] estate_ffb: #835 1. Date constraint, 2. Onchange employee list base on selected team, 3. Change double precision config to (4,0), 4. Move Plantation Year before TPH.

--- estate_ffb/models/estate_ffb.py
+++ estate_ffb/models/estate_ffb.py
@@ -1,5 +1,11 @@
-from openerp import models, fields, api, exceptions
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+from openerp import models, fields, api, exceptions, _
+from openerp.exceptions import ValidationError
+
 import openerp.addons.decimal_precision as dp
+
 
 class EstateFFB(models.Model):
     
@@ -9,6 +15,7 @@ class EstateFFB(models.Model):
     
     name = fields.Char("Name", required=True, store=True, default='New')
     date = fields.Date("Date", default=fields.Date.context_today, required=True)
+    max_day = fields.Integer("Date Constraint", help='Change to override maximum harvest date day(s) configuration')
     clerk_id = fields.Many2one('hr.employee', string="Clerk", required=True)  # constrains: if team_id.assistant_id = true
     team_ids = fields.Many2many('estate.hr.team', string="Team", required=True)
     team_member_ids = fields.One2many('estate.hr.member',  
@@ -26,6 +33,29 @@ class EstateFFB(models.Model):
                               ('rejected', 'Rejected')], "State", default="draft", track_visibility="onchange")
     description = fields.Text("Description")
     ffb_detail_ids = fields.One2many('estate.ffb.detail', string='FFB Line', inverse_name='ffb_id')
+
+    @api.one
+    @api.constrains('date')
+    def _check_date(self):
+        """Harvest date should be limited. Didn't support different timezone
+        Condition:
+        1. Zero value of max entry day = today transaction should be entry today.
+        2. Positive value of max entry day = allowed back/future dated transaction.
+        """
+        if self.date:
+            fmt = '%Y-%m-%d'
+            d1 = datetime.strptime(self.date, fmt)
+            d2 = datetime.strptime(fields.Date.today(), fmt)
+            delta = (d2 - d1).days
+            if self.max_day == 0 and abs(delta) > self.max_day:
+                error_msg = _("Transaction date should be today")
+                raise ValidationError(error_msg)
+            elif self.max_day != 0 and abs(delta) > self.max_day:
+                error_msg = _(
+                    "Transaction date should not be less than/greater than or equal to %s day(s)" % self.max_day)
+                raise ValidationError(error_msg)
+            else:
+                return True
     
 class EstateFFBDetail(models.Model):
     
@@ -37,6 +67,8 @@ class EstateFFBDetail(models.Model):
     upkeep_date = fields.Date(related='ffb_id.date', string='Date', store=True)
     employee_id = fields.Many2one('hr.employee', 'Harvester', required=True, track_visibility='onchange',
                                   domain=[('contract_type', 'in', ['1', '2'])])
+    # employee_id = fields.Many2one('hr.employee', 'Harvester', required=True, track_visibility='onchange',
+    #                               domain=[('contract_type', 'in', ['1', '2'])])
     employee_nik = fields.Char(related='employee_id.nik_number', string="Employee Identity Number ", store=True)
     employee_company_id = fields.Many2one(related='employee_id.company_id', string='Employee Company', store=True,
                                           help="Company of employee")
@@ -50,15 +82,15 @@ class EstateFFBDetail(models.Model):
     planted_year_id = fields.Many2one(related='location_id.planted_year_id', 
                                       string='Planted Year', store=True)
     qty_n = fields.Float('Ripe (N)', track_visibility='onchange',
-                                       help='Define ripe ffb', digits=dp.get_precision('Product Unit of Measure'))
+                                       help='Define ripe ffb', digits=(4,0))
     qty_a = fields.Float('Unripe (A)', track_visibility='onchange',
-                                       help='Define unripe ffb', digits=dp.get_precision('Product Unit of Measure'))
+                                       help='Define unripe ffb', digits=(4,0))
     qty_e = fields.Float('Overripe (E)', track_visibility='onchange',
-                                       help='Define overripe ffb', digits=dp.get_precision('Product Unit of Measure'))
+                                       help='Define overripe ffb', digits=(4,0))
     qty_l = fields.Float('Long Stalk (L)', track_visibility='onchange',
-                                       help='Define long stalk ffb', digits=dp.get_precision('Product Unit of Measure'))
+                                       help='Define long stalk ffb', digits=(4,0))
     qty_b = fields.Float('Loose Fruit (B)', track_visibility='onchange',
-                                       help='Define loose ffb', digits=dp.get_precision('Product Unit of Measure'))
+                                       help='Define loose ffb', digits=(4,0))
     flag_s = fields.Boolean('Not Stacked (S)', track_visibility='onchange',
                                        help='Define not stacked status ffb')
     flag_k = fields.Boolean('Not on Sack (K)', track_visibility='onchange',
@@ -77,3 +109,20 @@ class EstateFFBDetail(models.Model):
         block = self.env['estate.block.template'].search([('inherit_location_id', '=', self.tph_id.inherit_location_id.location_id.location_id.id)])
         if len(block):
             self.location_id = block[0]
+
+    @api.onchange('employee_id')
+    def _onchange_employee_id(self):
+        """
+        Define harvesting employee based on selected team_ids.
+        """
+        team_ids = self.ffb_id.team_ids.ids
+        if team_ids:
+            current_worker = []
+            hr_member_ids = self.env['estate.hr.member'].search([('team_id', 'in', team_ids)])
+            for member in hr_member_ids:
+                current_worker.append(member.employee_id.id)
+            return {
+                'domain': {
+                    'employee_id': [('id', 'in', current_worker)]
+                }
+            }
